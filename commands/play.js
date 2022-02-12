fb943d9e32e7fd3ac4071c713caf9f3b6ed0a7cd
Retro <70205403+retrouser955@users.noreply.github.com>
2022-02-12 12:28:43 +0630

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -13,7 +13,10 @@ module.exports = {
 		),
   async execute(interaction, player, playdl) {
     if (!interaction.member.voice.channelId) return interaction.reply({ content: "You are not in a voice channel!", ephemeral: true });
-      if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) return interaction.reply({ content: "You are not in my voice channel!", ephemeral: true });
+    if (interaction.guild.me.voice.channelId && interaction.member.voice.channelId !== interaction.guild.me.voice.channelId) return interaction.reply({ content: "You are not in my voice channel!", ephemeral: true });
+    if(!interaction.guild.me.permissions.has(Permissions.FLAGS.VIEW_CHANNEL) || interaction.guild.me.permissions.has(Permission.FLAGS.SEND_MESSAGES)) return interaction.reply({ content: "I do not have permissions to send messages or view this channel", ephemeral: true })
+    const songName = interaction.options.get("song").value
+    if(String(songName).toLowerCase().startsWith('https://spotify.com')) return interaction.reply({ content: "Spotify is not supported at the moment!", ephemeral: true })
       await interaction.deferReply();
       const queue = player.createQueue(interaction.guild, {
         metadata: {
@@ -24,10 +27,6 @@ module.exports = {
           if (source === "youtube") {
             return (await playdl.stream(track.url, { discordPlayerCompatibility : true })).stream;
           }
-	  if (source === "spotify") {
-            await interaction.channel.send(`Streaming from spotify is not supported. The bot maintainer (Retro#2448) will fix it ASAP\n Replying to ${interaction.author}'s request`)
-	    return (await playdl.stream("https://www.youtube.com/watch?v=jhFDyDgMVUI", { discordPlayerCompatibility : true } )).stream;
-	  }
         }
       })
       try {
@@ -37,7 +36,6 @@ module.exports = {
         queue.destroy();
         return await interaction.followUp({ content: "Could not join your voice channel!", ephemeral: true });
       }
-      const songName = interaction.options.get("song").value
       const track = await player.search(songName, {
         requestedBy: interaction.user
       }).then(x => x.tracks[0]);
